924b32a766517c32ecb0624cf3755b2b8f5d315d
Nimrah009 <nimrah2019@namal.edu.pk>
2025-06-11 07:47:25 -0700

    initial commit

---
+++ StudentPortalWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentPortalWeb.Models.Entities;
+
+namespace StudentPortalWeb.Data
+{
+    // This Class is a "bridge" between my application and the database.
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+        
+    }
+}
